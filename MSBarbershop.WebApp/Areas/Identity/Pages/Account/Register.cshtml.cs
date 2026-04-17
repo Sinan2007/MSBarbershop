a68e4850f6d37e829f79f80c771a209405361e58
Sinan2007 <sinancetalak@gmail.com>
2026-04-17 17:44:39 +0300

    added when a new person registers to automaticly be with Customer role

--- MSBarbershop.WebApp/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ MSBarbershop.WebApp/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -127,6 +127,7 @@ namespace MSBarbershop.WebApp.Areas.Identity.Pages.Account
 
                 if (result.Succeeded)
                 {
+                    await _userManager.AddToRoleAsync(user, "Customer");
                     _logger.LogInformation("User created a new account with password.");
 
                     var userId = await _userManager.GetUserIdAsync(user);
